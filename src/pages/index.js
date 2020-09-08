3347468b8f82c267d0213d8e38bf11b295a08e43
Andrew Davisson <akdavisson4@gmail.com>
2020-09-08 09:56:36 -0600

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from 'gatsby'
-import Header from '../components/header'
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 const Home = () => {
   return (
-    <div style={{ color: 'purple' }}>
+    <div style={{ color: "purple" }}>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
