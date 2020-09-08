b8953f95549a9c1222718b5e2db460da51c21d6e
Andrew Davisson <akdavisson4@gmail.com>
2020-09-08 09:51:18 -0600

    formatting

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react"
+import Header from "../components/header"
 
 const About = () => {
   return (
-    <div style={{ color: 'teal' }}>
+    <div style={{ color: "teal" }}>
       <h1>About Gatsby</h1>
       <p>Such wow. Very React.</p>
     </div>
-  );
+  )
 }
- 
-export default About;
+
+export default About
